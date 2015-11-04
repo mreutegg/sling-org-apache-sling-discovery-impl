mreutegg/sling-org-apache-sling-discovery-impl

commit 4366ef8b7de9fb0e7d1ecb58ce2e014db17c48a4

SLING-5243 : adding a retry-loop for the newly-introduced exception handling around establishedView.matches. Reason for this: looks like during testing you can run into a race-condition where the establishedView is just being created and only half visible - in that case this matches() throws an Exception even though the view will just be there - so doing a 5x500ms retry loop should handle this case. And since this is probably only a testing artifact, it shouldn't have any influence on real usage
git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1712584 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/sling/discovery/impl/common/heartbeat/HeartbeatHandler.java
@@ -598,13 +598,29 @@ public class HeartbeatHandler extends BaseViewChecker {
                 establishedViewMatches = false;
             } else {
                 String mismatchDetails;
-                try{
-                    mismatchDetails = establishedView.matches(liveInstances);
-                } catch(Exception e) {
-                    logger.error("doCheckViewWith: could not compare established view with live ones: "+e, e);
-                    invalidateCurrentEstablishedView();
-                    discoveryServiceImpl.handleTopologyChanging();
-                    return;
+                int retries = 0;
+                while(true) { // a small retry-loop
+                    try{
+                        mismatchDetails = establishedView.matches(liveInstances);
+                        break;
+                    } catch(Exception e) {
+                        logger.error("doCheckViewWith: could not compare established view with live ones: "+e, e);
+                        if (retries++<5) {
+                            // retry
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e1) {
+                                logger.info("doCheckViewWith: got interrupted");
+                            }
+                            logger.info("doCheckViewWith: retrying [retries="+retries+"]");
+                            continue;
+                        }
+                        // after 5 retires however
+                        logger.info("doCheckViewWith: invalidating view due to not being able to compare");
+                        invalidateCurrentEstablishedView();
+                        discoveryServiceImpl.handleTopologyChanging();
+                        return;
+                    }
                 }
                 if (mismatchDetails != null) {
                     logger.info("doCheckView: established view does not match. (details: " + mismatchDetails + ")");

src/main/java/org/apache/sling/discovery/impl/cluster/voting/VotingView.java
@@ -128,7 +128,7 @@ public class VotingView extends View {
             resourceResolver.create(membersResource, memberId, properties);
         }
         resourceResolver.commit();
-        logger.info("newVoting: new voting started: newViewId="+newViewId+", resource="+votingResource+", #members: "+liveInstances.size()+", members: "+liveInstances);
+        logger.info("newVoting: new voting started: newViewId="+newViewId+", initiatorId="+initiatorId+", resource="+votingResource+", #members: "+liveInstances.size()+", members: "+liveInstances);
         return new VotingView(votingResource);
     }
 

commit 76f4e7b15d124494809deed675ace6a6e1611663

SLING-5243 : reverting previous commit rev r1712584 plus also moving retrieving of the establishedView back to where it was prior to SLING-5195 (rev 1711257) : reason is that SLING-5195 introduced a regression which resulted in the rather ugly retry-loop introduced in r1712584. Now reverting that retry-loop and actually fixing the regression of rev1711257 by retrieving the establishedView *after* checking for ongoing votes - cos otherwise a nasty race condition can show up
git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1712651 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/sling/discovery/impl/common/heartbeat/HeartbeatHandler.java
@@ -557,9 +557,6 @@ public class HeartbeatHandler extends BaseViewChecker {
             }
         }
 
-        final View establishedView = ViewHelper.getEstablishedView(resourceResolver, config);
-        lastEstablishedViewId = establishedView == null ? null : establishedView.getResource().getName();
-
         final VotingView winningVoting = VotingHelper.getWinningVoting(
                 resourceResolver, config);
         int numOpenNonWinningVotes = VotingHelper.listOpenNonWinningVotings(
@@ -586,6 +583,8 @@ public class HeartbeatHandler extends BaseViewChecker {
         final Set<String> liveInstances = ViewHelper.determineLiveInstances(
                 clusterNodesRes, config);
 
+        final View establishedView = ViewHelper.getEstablishedView(resourceResolver, config);
+        lastEstablishedViewId = establishedView == null ? null : establishedView.getResource().getName();
         boolean establishedViewMatches;
         if (lastEstablishedViewId != null && failedEstablishedViewId != null
                 && lastEstablishedViewId.equals(failedEstablishedViewId)) {
@@ -598,29 +597,13 @@ public class HeartbeatHandler extends BaseViewChecker {
                 establishedViewMatches = false;
             } else {
                 String mismatchDetails;
-                int retries = 0;
-                while(true) { // a small retry-loop
-                    try{
-                        mismatchDetails = establishedView.matches(liveInstances);
-                        break;
-                    } catch(Exception e) {
-                        logger.error("doCheckViewWith: could not compare established view with live ones: "+e, e);
-                        if (retries++<5) {
-                            // retry
-                            try {
-                                Thread.sleep(500);
-                            } catch (InterruptedException e1) {
-                                logger.info("doCheckViewWith: got interrupted");
-                            }
-                            logger.info("doCheckViewWith: retrying [retries="+retries+"]");
-                            continue;
-                        }
-                        // after 5 retires however
-                        logger.info("doCheckViewWith: invalidating view due to not being able to compare");
-                        invalidateCurrentEstablishedView();
-                        discoveryServiceImpl.handleTopologyChanging();
-                        return;
-                    }
+                try{
+                    mismatchDetails = establishedView.matches(liveInstances);
+                } catch(Exception e) {
+                    logger.error("doCheckViewWith: could not compare established view with live ones: "+e, e);
+                    invalidateCurrentEstablishedView();
+                    discoveryServiceImpl.handleTopologyChanging();
+                    return;
                 }
                 if (mismatchDetails != null) {
                     logger.info("doCheckView: established view does not match. (details: " + mismatchDetails + ")");